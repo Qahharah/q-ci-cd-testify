19d4b574e1138d4ecfde53de7ab343a7458548de
Qahharat Ibrahim <ibrahimamirah08@gmail.com>
2025-11-27 14:44:53 +0100

    Added task 9 solution

---
+++ Module4/4A/task9.js
@@ -0,0 +1,7 @@
+// Function to calculate the area of a rectangle
+function calculateAreaOfRectangle(length, width) {
+    const area = length * width;
+    console.log("The area of the rectangle is", area);
+}
+
+calculateAreaOfRectangle(10, 20);
