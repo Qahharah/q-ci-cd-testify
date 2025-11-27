e9c60de0c15fd4929742132d8ccd447af6b394da
Qahharat Ibrahim <ibrahimamirah08@gmail.com>
2025-11-27 14:11:28 +0100

    Added task 8 solution

---
+++ Module4/4A/task8.js
@@ -0,0 +1,7 @@
+// Print all odd numbers between 1 and 20 using a for loop
+
+for (let i = 1; i <= 20; i++) {
+    if (i % 2 !== 0) { // Check if the number is odd
+        console.log(i);
+    }
+}
