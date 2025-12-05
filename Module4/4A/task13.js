3f25dd1a38719c4bf1bea7a1a5dd0d21359a6809
Qahharat Ibrahim <ibrahimamirah08@gmail.com>
2025-12-05 11:50:16 +0100

    Added task 13 solution

---
+++ Module4/4A/task13.js
@@ -0,0 +1,11 @@
+// Task 13: My Personal Library
+
+const books = {
+    title: "Ego Is the Enemy",
+    description: "A powerful book about overcoming the biggest obstacle to success and growth which is our own ego.",
+    numberOfPages: 256,
+    author: "Ryan Holiday",
+    reading: true
+};
+
+console.log(books);
