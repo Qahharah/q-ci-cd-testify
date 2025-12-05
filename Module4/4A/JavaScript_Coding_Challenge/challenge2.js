6346ca75a4b47896d624b4024b778c86e4ed176a
Qahharat Ibrahim <ibrahimamirah08@gmail.com>
2025-12-05 18:27:42 +0100

    Added challenge 2 solution

---
+++ Module4/4A/JavaScript_Coding_Challenge/challenge2.js
@@ -0,0 +1,12 @@
+// Challenge 2: Length Converter Function
+
+function convertKmToMeters(kilometers) {
+    const meters = kilometers * 1000;
+    return meters;
+}
+
+// Test
+const kmValue = 7; 
+console.log(convertKmToMeters(kmValue)); 
+
+// Expected output: 7000
