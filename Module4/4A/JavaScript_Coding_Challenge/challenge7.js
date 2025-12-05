8a66dacd326a8291a8dab53a75a0209fe470dc4b
Qahharat Ibrahim <ibrahimamirah08@gmail.com>
2025-12-05 19:10:40 +0100

    updated challenge 7

--- Module4/4A/JavaScript_Coding_Challenge/challenge7.js
+++ Module4/4A/JavaScript_Coding_Challenge/challenge7.js
@@ -1,18 +1,11 @@
-// Challenge 7: Filter out negative numbers
+// Challenge 7: Sort numbers in descending order
 
-function filterNegativeNumbers(arr) {
-    let result = [];
-
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] >= 0) {
-            result.push(arr[i]);
-        }
-    }
-
-    return result;
+function sortDescending(numbers) {
+    return numbers.sort((a, b) => b - a);
 }
 
 // Test
-const numbers = [-5, 10, -3, 20, 0, -1, 15];
-console.log(filterNegativeNumbers(numbers));
-// Expected output: [10, 20, 0, 15]
+const nums = [10, 3, 25, 7, 100, 42];
+console.log(sortDescending(nums));  
+
+// Expected: [100, 42, 25, 10, 7, 3]
