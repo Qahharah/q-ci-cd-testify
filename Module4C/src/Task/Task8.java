5c8a088829bb671c03ecc5f0c8d8424f4bc9daf4
Qahharat Ibrahim <ibrahimamirah08@gmail.com>
2026-01-14 16:39:12 +0100

    Simple interest calculator

---
+++ Module4C/src/Task/Task8.java
@@ -0,0 +1,25 @@
+package Task;
+
+import java.util.Scanner;
+
+public class Task8 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter Principal amount: ");
+        double principal = scanner.nextDouble();
+
+        System.out.print("Enter Rate of interest: ");
+        double rate = scanner.nextDouble();
+
+        System.out.print("Enter Time (in years): ");
+        double time = scanner.nextDouble();
+
+        double simpleInterest = (principal * rate * time) / 100;
+
+        System.out.println("Simple Interest is: " + simpleInterest);
+
+        scanner.close();
+    }
+}
