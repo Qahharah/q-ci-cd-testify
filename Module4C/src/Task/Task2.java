aefb6c5c8da61360774abfc5a66f139378a94aa7
Qahharat Ibrahim <ibrahimamirah08@gmail.com>
2026-01-14 07:16:33 +0100

    Print six variables of different data types

---
+++ Module4C/src/Task/Task2.java
@@ -0,0 +1,20 @@
+package Task;
+
+public class Task2 {
+    public static void main(String[] args) {
+
+        int age = 25;
+        double salary = 150000.50;
+        char grade = 'A';
+        boolean isActive = true;
+        String name = "Testify Student";
+        long accountNumber = 1234567890L;
+
+        System.out.println("Age: " + age);
+        System.out.println("Salary: " + salary);
+        System.out.println("Grade: " + grade);
+        System.out.println("Active: " + isActive);
+        System.out.println("Name: " + name);
+        System.out.println("Account Number: " + accountNumber);
+    }
+}
