be7140605f9f4efac18f544e5e0628038d1184e9
Qahharat Ibrahim <ibrahimamirah08@gmail.com>
2026-01-15 14:53:18 +0100

    Login interface and tester implementation

---
+++ Module4C/src/Task/Task17BLogin.java
@@ -0,0 +1,12 @@
+package Task;
+
+public interface Task17BLogin {
+
+    void enterUsername(String username);
+
+    void enterPassword(String password);
+
+    void clickLogin();
+
+    void verifyDashboardAccess();
+}
